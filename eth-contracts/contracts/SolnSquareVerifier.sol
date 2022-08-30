20a6b9c304253aa63d0f3b86a02cd9db31b40de9
jvalero <jvalero84@gmail.com>
2022-08-30 23:02:35 +0200

    SolnSquareVerifier WIP

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -9,29 +9,72 @@ import "./ERC721Mintable.sol";
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 contract SolnSquareVerifier is ERC721MintableComplete {
 
+  Verifier verifier;
 
-  
+  // TODO define a solutions struct that can hold an index & an address
+  struct Solutions {
+    uint[2] a;
+    uint[2][2] b;
+    uint[2] c;
+    uint[2] input;
+  }
+
+  // TODO define an array of the above struct
+
+
+  // TODO define a mapping to store unique solutions submitted
+  mapping(bytes32 => Solutions) private submittedSolutions;
+
+  // TODO Create an event to emit when a solution is added
+  event solutionAdded(address submitter);
+
+  // TODO Create a function to add the solutions to the array and emit the event
+  function addSolution(uint[2] memory a,
+                       uint[2][2] memory b,
+                       uint[2] memory c,
+                       uint[2] memory input)
+                       public
+  {
+
+    bytes32 solHash = keccak256(abi.encodePacked(a, b, c, input));
+
+    Solutions memory solution = Solutions({
+                                              a: a,
+                                              b: b,
+                                              c: c,
+                                              input: input
+                                            });
+
+    submittedSolutions[solHash] = solution;
+    emit solutionAdded(msg.sender);
+
+  }
+
+
 }
 
 
-// TODO define a solutions struct that can hold an index & an address
-
-
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
 
 
 
-// TODO Create an event to emit when a solution is added
 
 
 
-// TODO Create a function to add the solutions to the array and emit the event
+
+
+
+
+
 
 
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
+
+
+
+
+contract Verifier {
+  function verifyTx(uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[2] memory input) public returns (bool);
+}
